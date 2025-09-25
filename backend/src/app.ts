a8300fecefbd058b16b8888217df1756d3491388
CLONDIREANU <clondireanug@gmail.com>
2025-09-25 13:49:51 +0200

    feat:implement route and controller for signup and login

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,8 +1,12 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import { authRouter } from './routes/authRoutes.ts'
 dotenv.config()
 const app = express()
 const PORT = process.env.PORT || 3000
+
+app.use("/", authRouter)
+
 app.listen(PORT, () =>{
     console.log(`Server running on port ${PORT}`)
 })
