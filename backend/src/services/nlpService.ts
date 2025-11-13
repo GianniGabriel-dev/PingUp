308b5f16b627f249e187c6da5b261aa654955d22
Gianni Gabriel <clondireanug@gmail.com>
2025-11-13 21:28:15 +0100

    feat:enhance sentiment analysis adding support to all languages

--- backend/src/services/nlpService.ts
+++ backend/src/services/nlpService.ts
@@ -1,37 +1,75 @@
 import { LanguageServiceClient } from "@google-cloud/language";
 //protos es para tipar las respuestas de la API
 import type { protos } from "@google-cloud/language";
+import { translateText } from "./translationService.js";
+import { Translate } from "@google-cloud/translate/build/src/v2/index.js";
 
 //se inicializa el cliente de la api, los datos para acceder a esta están en el archivo oculto json con las credenciales de google
-const client= new LanguageServiceClient();
+const client = new LanguageServiceClient();
+const translate = new Translate();
 
+//la funcion se divide en dos partes, primero se analiza el sentimiento del texto, si el idioma no es soportado por la API de nlp de google se traduce al ingles y se vuelve a analizar
 export async function analyzeSentiment(text: string): Promise<{
   score: number;
   magnitude: number;
-  //como el nlp de google devuelve tambien el lenguaje, me aprovecho de el para añadirselo al post creado
-  language: string
+  language: string;
 }> {
-  const document: protos.google.cloud.language.v1.IDocument = {
-    content: text,
-    type: "PLAIN_TEXT",
-  };
+  let sentiment = null;
+  let language = "und";
 
-  const [result] = await client.analyzeSentiment({ document });
-  console.log(result)
-  const sentiment = result.documentSentiment;
-  const language= result.language ?? "und"
-  console.log(sentiment)
+  //primero se detecta el idioma del texto
+  const [detection] = await translate.detect(text);
+  language = detection.language || "und";
 
+  try {
+    //se crea el documento con el texto a analizar
+    const document: protos.google.cloud.language.v1.IDocument = {
+      content: text,
+      type: "PLAIN_TEXT",
+    };
+    //Se analiza el sentimiento del texto
+    const [result] = await client.analyzeSentiment({ document });
+    sentiment = result.documentSentiment;
+    //el ? se usa para evitar errores en caso de que result sea undefined
+    language = result?.language ?? "und";
+
+    console.log(result);
+    console.log(sentiment);
+  } catch (error: any) {
+    //si el idioma no es soportado por la API de nlp de google devuelve un error con code 3, caundo esto pasa se traduce el texto al ingles y se vuelve a analizar
+    if (error.code === 3) {
+      const { translation } = await translateText(text, "en");
+      //se analiza el sentimiento del texto traducido al inglés
+      const [translatedResult] = await client.analyzeSentiment({
+        document: { content: translation, type: "PLAIN_TEXT" },
+      });
+      sentiment = translatedResult.documentSentiment;
+
+      console.log(translatedResult);
+      console.log(sentiment);
+    } else {
+      throw error;
+    }
+  }
   return {
-    score: sentiment?.score ?? 0, 
+    score: sentiment?.score ?? 0,
     magnitude: sentiment?.magnitude ?? 0,
-    language
+    language,
   };
 }
 
 //se establece el umbral de sentimientos para calificar el tono correctamente
-export function getSentimentLabel(score: number): "positivo" | "neutral" | "negativo" {
+export function getSentimentLabel(
+  score: number
+): "positivo" | "neutral" | "negativo" {
   if (score > 0.25) return "positivo";
   if (score < -0.35) return "negativo";
   return "neutral";
 }
+//importante para evitar warnings innecesarios de la librería
+const originalWarn = console.warn;
+console.warn = (...args) => {
+  if (!args[0]?.includes("fromJSON") && !args[0]?.includes("fromStream")) {
+    originalWarn(...args);
+  }
+};
--- backend/src/services/translationService.ts
+++ backend/src/services/translationService.ts
@@ -1,40 +1,61 @@
 import { Translate } from "@google-cloud/translate/build/src/v2/index.js";
-import { createTranslation, existingTranslationPost, getContentById } from "../queries/translationQueries.js";
-
+import {
+  createTranslation,
+  existingTranslationPost,
+  getContentById,
+} from "../queries/translationQueries.js";
 
 //se inicializa el cliente de la api, los datos para acceder a esta están en el archivo oculto json con las credenciales de google
 const translate = new Translate();
 
 //target es al idioma al que quieres traducir el texto
-export async function translateText(text:string, target:string):Promise<{
+export async function translateText(
+  text: string,
+  target: string
+): Promise<{
   translation: string;
   detectedSourceLanguage: string;
-}>{
+}> {
   let [translation, response] = await translate.translate(text, target);
 
-  const detectedSourceLanguage = response.data.translations[0].detectedSourceLanguage;
-  return {translation,detectedSourceLanguage};
+  const detectedSourceLanguage =
+    response.data.translations[0].detectedSourceLanguage;
+  return { translation, detectedSourceLanguage };
 }
 
 export async function translatePostContent(post_id: number, target: string) {
   //se obtiene el content del post
   const text = await getContentById(post_id);
   //se comprueba si ya existe una traducción del post en el idioma objetivo
-  const existingTranslation = await existingTranslationPost(post_id)
+  const existingTranslation = await existingTranslationPost(post_id);
 
   if (!text) throw new Error("Post no encontrado");
-  
+
   //si ya existe una traduccion o el post ya está en el idioma target, no se traduce
-  if(text.language === target || existingTranslation) throw new Error("El post ya está en el idioma objetivo o ya se ha creado una traducción en este idioma");
-  
-  else{
+  if (text.language === target || existingTranslation)
+    throw new Error(
+      "El post ya está en el idioma objetivo o ya se ha creado una traducción en este idioma"
+    );
+  else {
     //se traduce el contenido del post
     const translation = await translateText(text.content, target);
     if (!translation) throw new Error("Error al traducir el texto");
     //se guarda la traducción en la base de datos
-    await createTranslation(post_id, text.content, translation.translation, target, translation.detectedSourceLanguage);
+    await createTranslation(
+      post_id,
+      text.content,
+      translation.translation,
+      target,
+      translation.detectedSourceLanguage
+    );
 
     return translation;
   }
-
 }
+//importante para evitar warnings innecesarios de la librería
+const originalWarn = console.warn;
+console.warn = (...args) => {
+  if (!args[0]?.includes("fromJSON") && !args[0]?.includes("fromStream")) {
+    originalWarn(...args);
+  }
+};
